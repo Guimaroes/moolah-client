392fee63bc83ac3178b04a7c512e3ceaa1558e70
Guimaroes <gabriel.guimaraes@universo.univates.br>
2021-06-29 00:19:10 -0300

    Ajusting refresh 404 error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { TypeListComponent } from './type-list/type-list.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatListModule } from '@angular/material/list';
-
 import { authInterceptorProviders } from './_helpers/auth.interceptor';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FinanceListComponent } from './finance-list/finance-list.component';
 @NgModule({
@@ -36,7 +36,7 @@ import { FinanceListComponent } from './finance-list/finance-list.component';
     MaterialModule,
     MatListModule
   ],
-  providers: [authInterceptorProviders],
+  providers: [authInterceptorProviders, {provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
